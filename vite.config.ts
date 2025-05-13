461004e415580ef983571ee61cfb8a40097dc75f
Ivan Francisco <hansivann23@gmail.com>
2025-05-12 20:13:50 -0700

    fix basename

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,10 @@ declare module "@remix-run/node" {
 }
 
 export default defineConfig({
+  base: "/D479-Prototype/",
   plugins: [
     remix({
+      basename: "/D479-Prototype/",
       ssr:false,
       future: {
         v3_fetcherPersist: true,
